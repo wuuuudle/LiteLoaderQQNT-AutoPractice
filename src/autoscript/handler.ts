b89d7eb8ce1d7ea088c7bf6fce056fc3721c1702
wuuuudle <wuuuudle@gmail.com>
2024-10-10 18:31:28 +0800

    修复，同机多开，at判断错误bug

--- src/autoscript/handler.ts
+++ src/autoscript/handler.ts
@@ -3,13 +3,14 @@ import {AutoScript} from './base';
 export function CreateHandler(processors: Record<string, AutoScript>) {
     return async (message: euphony.MessageChain, source: euphony.MessageSource) => {
         const config = await LiteLoader.api.config.get('xiaoxiao_auto_practice') as Config;
+        const name = euphony.Group.make(config.groupId).getMemberFromUin(euphony.Client.getUin()).getCardName();
         if (config.autoPractice) {
             const concat = source.getContact();
             if ((concat instanceof euphony.Member) &&   // 是成员消息
                 (concat.getGroup().getId() === config.groupId) &&  // 是修仙群的消息
                 (concat.getId() === config.xiaoxiaoId)) { // 是小小的消息
                 const convertMessages = message.contentToString();
-                const isAtMe = convertMessages.some(item => item === `@${config.myName}` || `@${euphony.Client.getUid()}`);
+                const isAtMe = convertMessages.some(item => (item === `@${name}`) || (item === `@${euphony.Client.getUid()}`));
                 if (isAtMe) {
                     await source.setMsgRead();
                     for (const item of Object.values(processors)) {
